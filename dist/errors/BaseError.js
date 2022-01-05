6714db3a4afed2f55d2d01654499f5b36565fdac
Ricky Rodrigo <rickyrodrigo.ph@gmail.com>
2022-01-05 20:47:39 +0800

    fix: accidentally deployed build test version

--- dist/errors/BaseError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = this.constructor.name;
-    }
-}
-exports.default = BaseError;
